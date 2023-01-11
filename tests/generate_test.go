8cefff4afc5a69a83c37ef472010e59470cda902
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-01-11 12:20:26 +0300

    Add sanity checks for generator output

--- tests/generate_test.go
+++ tests/generate_test.go
@@ -4,12 +4,24 @@ import (
 	"github.com/sio/coolname"
 	"testing"
 
-	"fmt"
+	"strings"
 )
 
 func TestGenerate(t *testing.T) {
 	var g coolname.Generator
 	words := g.Generate()
-	fmt.Println(words)
-	t.Fatal("TODO")
+	if len(words) < 4 {
+		t.Errorf("output too short: %v", words)
+	}
+	for i := 0; i < len(words); i++ {
+		if len(words[i]) == 0 {
+			t.Errorf("word %d is empty: %v", i, words)
+		}
+		if strings.Contains(words[i], " ") {
+			t.Errorf("word %d contains a space: %q", i, words[i])
+		}
+		if strings.Contains(words[i], "=") {
+			t.Errorf("word %d contains an equal sign: %q", i, words[i])
+		}
+	}
 }
